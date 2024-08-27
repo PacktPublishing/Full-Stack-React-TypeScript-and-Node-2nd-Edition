c1e2a10029eb54bc230d1f60618a86a97ae8a778
David Choi <dharric@live.com>
2024-08-27 13:17:22 -0400

    finished adding follow route tests

--- Final/blog/server/src/routes/FollowRoutes.test.ts
+++ Final/blog/server/src/routes/FollowRoutes.test.ts
@@ -65,6 +65,51 @@ describe("POST /follow/followers", () => {
         assert.equal(followers.length, 10);
       });
   });
+
+  it("should return next page of followers", async () => {
+    const followed = await repo.Profile.insertProfile(
+      faker.internet.userName(),
+      faker.internet.displayName(),
+      faker.lorem.sentence(5),
+      faker.lorem.sentence(6),
+      faker.internet.url(),
+      faker.internet.url(),
+      getAvatar()
+    );
+    const followersCount = 10;
+    const followerProfileIds: bigint[] = new Array(followersCount);
+    for (let i = 0; i < followersCount; i++) {
+      const followerProfile = await repo.Profile.insertProfile(
+        faker.internet.userName(),
+        faker.internet.displayName(),
+        faker.lorem.sentence(5),
+        faker.lorem.sentence(6),
+        faker.internet.url(),
+        faker.internet.url(),
+        getAvatar()
+      );
+      followerProfileIds[i] = followerProfile.id;
+      await repo.Follow.insertFollow(followed.id, followerProfile.id);
+    }
+    const firstFive = await repo.Follow.selectFollowers(followed.id, 5);
+    const lastCursor = firstFive[firstFive.length - 1].followId;
+
+    await request(app)
+      .post("/follow/followers")
+      .send({
+        profileId: serializeBigInt(followed.id),
+        pageSize: 5,
+        lastCursor: serializeBigInt(lastCursor),
+      })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        assert.equal(res.statusCode, 200);
+        const followers: ProfileModel[] = res.body;
+        assert.equal(followers.length, 5);
+        assert.equal(followers[followers.length - 1].id, followerProfileIds[0]);
+      });
+  });
 });
 
 describe("POST /follow/followed", () => {
@@ -107,6 +152,52 @@ describe("POST /follow/followed", () => {
         assert.equal(res.statusCode, 200);
         const followed: ProfileModel[] = res.body;
         assert.equal(followed.length, 10);
+      });
+  });
+
+  it("should return next page of followed", async () => {
+    const follower = await repo.Profile.insertProfile(
+      faker.internet.userName(),
+      faker.internet.displayName(),
+      faker.lorem.sentence(5),
+      faker.lorem.sentence(6),
+      faker.internet.url(),
+      faker.internet.url(),
+      getAvatar()
+    );
+
+    const followedCount = 10;
+    const followedIds: bigint[] = new Array(followedCount);
+    for (let i = 0; i < followedCount; i++) {
+      const followed = await repo.Profile.insertProfile(
+        faker.internet.userName(),
+        faker.internet.displayName(),
+        faker.lorem.sentence(5),
+        faker.lorem.sentence(6),
+        faker.internet.url(),
+        faker.internet.url(),
+        getAvatar()
+      );
+      followedIds[i] = followed.id;
+      await repo.Follow.insertFollow(followed.id, follower.id);
+    }
+    const firstFive = await repo.Follow.selectFollowed(follower.id, 5);
+    const lastCursor = firstFive[firstFive.length - 1].followId;
+
+    await request(app)
+      .post("/follow/followed")
+      .send({
+        profileId: serializeBigInt(follower.id),
+        pageSize: 5,
+        lastCursor: serializeBigInt(lastCursor),
+      })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        assert.equal(res.statusCode, 200);
+        const followed: ProfileModel[] = res.body;
+        assert.equal(followed.length, 5);
+        assert.equal(followed[followed.length - 1].id, followedIds[0]);
       });
   });
 });
