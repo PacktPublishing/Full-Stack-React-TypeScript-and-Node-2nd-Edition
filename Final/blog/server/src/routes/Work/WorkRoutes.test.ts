21a8810b23d60480030b85cc41b4f21776fd816e
David Choi <dharric@live.com>
2024-09-04 16:28:01 -0400

    added route test for getWork

--- Final/blog/server/src/routes/Work/WorkRoutes.test.ts
+++ Final/blog/server/src/routes/Work/WorkRoutes.test.ts
@@ -44,3 +44,63 @@ describe("POST /work/new", () => {
       });
   });
 });
+
+describe("GET /work/:id", () => {
+  it("get work", async () => {
+    const profile = await repo.Profile.insertProfile(
+      faker.internet.userName(),
+      faker.internet.displayName(),
+      faker.lorem.sentence(2),
+      faker.internet.url(),
+      faker.internet.url(),
+      avatars[0]
+    );
+    const topic = await repo.Topic.insertTopic(faker.company.name());
+
+    const title = faker.lorem.sentence(1);
+    const description = faker.lorem.sentence(2);
+    const content = faker.lorem.sentence(4);
+    const authorId = serializeBigInt(profile.id);
+    const topicId = serializeBigInt(topic.id);
+    const work = await repo.Work.insertWork(
+      title,
+      description,
+      content,
+      profile.id,
+      [topic.id],
+      [
+        {
+          imagePlaceholder: "A",
+          image: avatars[0],
+        },
+        {
+          imagePlaceholder: "B",
+          image: avatars[1],
+        },
+      ]
+    );
+
+    await request(app)
+      .get(`/work/${work.id}`)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then(async (res) => {
+        const returnedWork: {
+          id: bigint;
+          title: string;
+          description: string;
+          content: string;
+          author: {
+            id: bigint;
+            userName: string;
+            fullName: string;
+            description: string;
+          };
+        } = res.body;
+        assert.equal(returnedWork?.title, title);
+        assert.equal(returnedWork?.description, description);
+        assert.equal(returnedWork?.content, content);
+        assert.equal(returnedWork?.author.id, authorId);
+      });
+  });
+});
