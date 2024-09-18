6802bf80ffde37d34bf3b9bf32a04eea1fe0e96b
David Choi <dharric@live.com>
2024-09-17 20:37:47 -0400

    new updates for chap5 proj

--- Chap5/components/src/App.tsx
+++ Chap5/components/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
+import Home, { UserType } from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [label, setLabel] = useState("");
+  const [users, setUsers] = useState<UserType[]>();
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${label}`).then(
+      (result) => {
+        result.json().then((users) => {
+          setUsers(users);
+        });
+      }
+    );
+  }, [label]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLabel(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      id: <input type="text" value={label} onChange={onChange} />
+      <Home users={users} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Chap5/components/src/Home.tsx
@@ -0,0 +1,31 @@
+import { useId } from "react";
+
+export type UserType = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+interface HomeProps {
+  users?: UserType[];
+}
+
+export default function Home({ users }: HomeProps) {
+  return users?.map((user) => {
+    return (
+      <div
+        key={useId()}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          marginBottom: "10px",
+        }}
+      >
+        <div>id: {user?.id}</div>
+        <div>title: {user?.title}</div>
+      </div>
+    );
+  });
+}
