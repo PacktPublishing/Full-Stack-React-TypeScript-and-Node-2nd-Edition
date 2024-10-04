7c4e5d6e4c33f6adb0d3cf78ba8768c7d1b482d1
David Choi <dharric@live.com>
2024-10-04 17:23:57 -0400

    update test chap7 App.test.tsx; update vite.config

---
+++ Chap7/vitest/src/App.test.tsx
@@ -0,0 +1,12 @@
+import { test, expect } from "vitest";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+
+  const linkElement = getByText(/learn react/i);
+
+  expect(linkElement).toBeInTheDocument();
+});
