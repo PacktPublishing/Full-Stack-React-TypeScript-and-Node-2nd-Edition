7bc7c76f06f4b94de7500569f425c4796332b8a8
David Choi <dharric@live.com>
2024-10-16 15:17:55 -0400

    add express routes

--- Chap9/express-server/expressapp.js
+++ Chap9/express-server/expressapp.js
@@ -8,22 +8,57 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log("First middleware.");
-
-  throw new Error("A failure occurred!");
+  next();
 });
 
 app.use(router);
 
-router.get("/a", (req, res, next) => {
-  res.send("Hello this is route a");
+router.get("/api/v1/users/:id", (req, res, next) => {
+  const users = [
+    {
+      id: 1,
+      username: "tom",
+    },
+
+    {
+      id: 2,
+      username: "jon",
+    },
+
+    {
+      id: 3,
+      username: "linda",
+    },
+  ];
+
+  console.log("id", req.params.id);
+
+  const user = users.find((usr) => usr.id == req.params.id);
+
+  res.send(`User ${user?.username}`);
 });
 
-router.post("/c", (req, res, next) => {
-  res.send(`Hello this is route c. Message is ${req.body.message}`);
-});
+router.post("/api/v1/groups", (req, res, next) => {
+  const groups = [
+    {
+      id: 1,
+      groupname: "Admins",
+    },
 
-app.use((err, req, res, next) => {
-  res.status(500).send(`Error: ${err.message}`);
+    {
+      id: 2,
+      groupname: "Users",
+    },
+
+    {
+      id: 3,
+      groupname: "Employees",
+    },
+  ];
+
+  const group = groups.find((grp) => grp.id == req.body.groupid);
+
+  res.send(`Group ${group.groupname}`);
 });
 
 app.listen({ port: 8000 }, () => {
