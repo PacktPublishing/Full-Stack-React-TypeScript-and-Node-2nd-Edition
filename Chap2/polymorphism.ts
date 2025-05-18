968f7bbd37640f1a50166057f9d6a5b5a6fbcf93
jsoneaday <1474935+jsoneaday@users.noreply.github.com>
2025-05-17 21:37:33 -0400

    add new file polymorphism (#17)

---
+++ Chap2/polymorphism.ts
@@ -0,0 +1,41 @@
+interface Animal {
+  name: string;
+
+  runMaxMiles(hours: number): number;
+}
+
+class Wolf implements Animal {
+  name: string = "";
+  runMaxMiles(hours: number): number {
+    return hours * 45;
+  }
+}
+
+class Cheetah implements Animal {
+  name: string = "";
+  runMaxMiles(hours: number): number {
+    return hours * 75;
+  }
+}
+
+const hours = 0.5;
+function pickTheBestAnimalToRun(hours: number): number {
+  let animal: Animal | undefined;
+  if (hours >= 0.5) {
+    animal = new Wolf();
+    animal.name = "wolfie";
+  } else {
+    animal = new Cheetah();
+    animal.name = "cheetose";
+  }
+
+  if (animal instanceof Wolf) {
+    console.log("This is a wolf");
+  }
+  if (animal instanceof Cheetah) {
+    console.log("This is a cheetah");
+  }
+  return animal.runMaxMiles(hours);
+}
+
+pickTheBestAnimalToRun(hours);
