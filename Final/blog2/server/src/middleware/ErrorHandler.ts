626062e73079118ba202592d7393f8babf690201
David Choi <dharric@live.com>
2025-07-24 12:14:05 -0400

    add test for ErrorHandler middleware

---
+++ Final/blog2/server/src/middleware/ErrorHandler.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express";
+import { logger } from "../lib/utils/Logger";
+
+export type ErrorHandlerSend = { error: string };
+
+export const errorHandler = async (err: any, _req: Request, res: Response) => {
+  logger.error(err.stack);
+  res.status(500).send({ error: "Internal Server Error" });
+};
